3cf2d4b3e492394049d1f98f59679bf05c03c828
Weihuang Wong <weihuang.wong@gmail.com>
2019-05-05 09:28:37 -0500

    Things.
    * Added padding to entity names and labels
    * Added option for entity label color
    * Removed string-wrapping

--- R/display_ent.R
+++ R/display_ent.R
@@ -1,18 +1,19 @@
 #' @importFrom utils getFromNamespace tail
 
 hilite <- function(x, deco = "red") {
-  deco <- getFromNamespace(deco, "crayon")
-  if (grepl(" $", x)) {
-    paste(deco(trimws(x)), " ")
-  } else {
-    deco(x)
-  }
+  .dec <- getFromNamespace(deco, "crayon")
+  tokens <- strsplit(x, " ")[[1]]
+  if (length(tokens) > 1)
+    tokens <- c(paste0(tokens[1:(length(tokens) - 1)], " "),
+      tail(tokens, 1))
+  paste(sapply(c(" ", tokens, " "), .dec), collapse = "")
 }
 
 #' Visualize entities in text parsed with spacyr.
 #'
 #' @param x     A spacyr_parsed object
-#' @param deco  A crayon style, given as a character string
+#' @param deco_ent  A crayon style for entity name, given as a character string
+#' @param deco_lab  A crayon style for entity label, given as a character string
 #' @return A character string with styled tokens
 #' @details x must include the "whitespace_" attribute. Therefore, \code{spacy_parse}
 #'   should be run with the \code{additional_attributes} argument:
@@ -21,36 +22,33 @@ hilite <- function(x, deco = "red") {
 #'   a styled character vector to the terminal.
 #' @examples
 #' \dontrun{
-#' txt <- "Kenneth Benoit is currently Professor of Computational Social
-#' Science in the Department of Methodology at the London School of
-#' Economics and Political Science. He has previously held positions in
-#' the Department of Political Science at Trinity College Dublin and at
-#' the Central European University (Budapest)."
-#' txt <- gsub("\n", " ", txt)
-#
+#' txt <- "Larry Page is the co-founder of Google, located in Mountain View, CA."
 #' parsed <- spacyr::spacy_parse(txt, additional_attributes = "whitespace_")
-#' display_ent(parsed, deco = "yellow")
+#' display_ent(parsed)
 #' }
 #' @export
-display_ent <- function(x, deco = "red") {
-  if (!("whitespace_" %in% names(x)))
-    stop('Dataframe must have "whitespace_" column; try spacy_parse(x, ..., additional_attributes = "whitespace_")')
+display_ent <- function(x, deco_ent = "bgYellow", deco_lab = "bgBlue") {
+  if (!inherits(x, "spacyr_parsed"))
+    stop('x must be output from spacyr_parsed')
+  if (!all(c("entity", "whitespace_") %in% names(x)))
+    stop('Dataframe must have "entity" and "whitespace_" columns; try spacy_parse(x, ..., entity = TRUE, additional_attributes = "whitespace_")')
+  .dec <- getFromNamespace(deco_lab, "crayon")
   # create token groups
   x$index <- 1:nrow(x)
   for (i in 2:nrow(x)) {
-    if (grepl("_I$", x$entity[i])) x$index[i] <- x$index[i-1]
+    if (grepl("_I$", x$entity[i])) x$index[i] <- x$index[i - 1]
   }
   groups <- split(x, x$index)
-  out <- unlist(lapply(groups, function(x) {
-    entity <- gsub("^(.*)_B$", "\\1", x$entity[1])
+  out <- unlist(lapply(groups, function(s) {
+    entity <- gsub("^(.*)_B$", "\\1", s$entity[1])
+    entity_label <- ""
     if (entity != "")
-      x$token[nrow(x)] <- paste(tail(x$token, 1), sprintf("[%s]", entity))
-    entity_token <- paste(paste0(x$token, x$whitespace_), collapse = "")
+      entity_label <- paste0(.dec(sprintf(" %s ", entity)), tail(s$whitespace_, 1))
+    entity_token <- paste(paste0(s$token, s$whitespace_), collapse = "")
     if (entity == "") return(entity_token)
-    hilite(entity_token, deco = deco)
+    paste0(hilite(entity_token, deco = deco_ent), entity_label)
   }))
-  out <- paste(out, collapse = "")
-  disp_out <- strwrap(out, width = 80)
+  disp_out <- paste(out, collapse = "")
   cat(disp_out, "\n", sep = "\n")
   invisible(out)
 }
